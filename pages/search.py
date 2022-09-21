1df850eb5b841b43a2593e37120171091e8ea4a4
Artem Kisel <artemkisel@yahoo.com>
2022-09-20 20:46:33 -0400

    Add test for auto-complete suggestions in the search drop-down for relevancy to user's search phrase. Fix #6

--- pages/search.py
+++ pages/search.py
@@ -1,11 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 
 class DuckDuckGoSearchPage:
 
     URL = 'https://www.duckduckgo.com'
+
     SEARCH_INPUT_FIELD_LOCATOR = (By.ID, 'search_form_input_homepage')
+    SEARCH_AUTOCOMPLETE_OPTIONS_LOCATOR = (By.CSS_SELECTOR, 'div.search__autocomplete div.acp')
 
     def __init__(self, browser, search_phrase=None):
         self.browser = browser
@@ -21,3 +24,14 @@ class DuckDuckGoSearchPage:
     def search(self):
         search_input_field = self.browser.find_element(*self.SEARCH_INPUT_FIELD_LOCATOR)
         search_input_field.send_keys(self.search_phrase + Keys.RETURN)
+
+    def get_autocomplete_suggestions(self):
+        search_input_field = self.browser.find_element(*self.SEARCH_INPUT_FIELD_LOCATOR)
+
+        search_input_field.send_keys(self.search_phrase)
+
+        # Not the best practice
+        time.sleep(1)
+
+        search_autocomplete_options = self.browser.find_elements(*self.SEARCH_AUTOCOMPLETE_OPTIONS_LOCATOR)
+        return search_autocomplete_options
